401923abb5de252426a17b35b70e0bc2bc308ed0
J.I. Cruz <acounts@jicruz.com>
2021-05-01 17:18:24 -0500

    Update heap.h

--- 0x02-huffman_coding/heap/heap.h
+++ 0x02-huffman_coding/heap/heap.h
@@ -38,5 +38,10 @@ typedef struct heap_s
 heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+void swap_node_with_parent(binary_tree_node_t *node);
+void heapify(binary_tree_node_t *node, heap_t *heap);
+void *heap_extract(heap_t *heap);
+
+
 
 #endif /* HEAP_H */
