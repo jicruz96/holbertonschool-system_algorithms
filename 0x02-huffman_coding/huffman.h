2d0e35ddb68a856b1258c4c2168f522b6acc0e9d
J.I. Cruz <acounts@jicruz.com>
2021-05-01 17:53:05 -0500

    Add symbol.c; update huffman.h

--- 0x02-huffman_coding/huffman.h
+++ 0x02-huffman_coding/huffman.h
@@ -9,10 +9,11 @@
  */
 typedef struct symbol_s
 {
-    char data;
-    size_t freq;
+	char data;
+	size_t freq;
 } symbol_t;
 
+symbol_t *symbol_create(char data, size_t freq);
 
 
 #endif /* HUFFMAN_H */
---
+++ 0x02-huffman_coding/symbol.c
@@ -0,0 +1,21 @@
+#include "huffman.h"
+#include <stdlib.h>
+
+/**
+ * symbol_create - creates a symbol_t data structure
+ * @data: data to be stored in structure
+ * @freq: freq to be stored in structure
+ * Return: pointer to the created structure
+ **/
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbol_t *new = malloc(sizeof(symbol_t));
+
+	if (new)
+	{
+		new->data = data;
+		new->freq = freq;
+	}
+
+	return (new);
+}
