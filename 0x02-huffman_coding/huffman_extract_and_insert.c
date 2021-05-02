f4544c0ca5af31602e8ed8e43577908d53fc75a5
jicruz96 <accounts@jicruz.com>
2021-05-02 18:12:05 +0000

    Add another malloc check to huffman_extract_and_insert.c()

--- 0x02-huffman_coding/huffman_extract_and_insert.c
+++ 0x02-huffman_coding/huffman_extract_and_insert.c
@@ -10,19 +10,24 @@ int huffman_extract_and_insert(heap_t *priority_queue)
 {
 	binary_tree_node_t *node1, *node2, *node;
 	size_t freq1, freq2;
+    symbol_t *symbol;
 
 	if (priority_queue == NULL || priority_queue->size < 2)
 		return (0);
 
 	node1       = heap_extract(priority_queue);
 	node2       = heap_extract(priority_queue);
-	if (!node1 || !node2)
+	if (node1 == NULL || node2 == NULL)
 		return (0);
 	freq1       = ((symbol_t *)node1->data)->freq;
 	freq2       = ((symbol_t *)node2->data)->freq;
 
-	node        = binary_tree_node(NULL, symbol_create(-1, freq1 + freq2));
-	if (!node)
+    symbol = symbol_create(-1, freq1 + freq2);
+    if (symbol == NULL)
+        return (0);
+
+	node        = binary_tree_node(NULL, symbol);
+	if (node == NULL)
 		return (0);
 	node->left  = node1, node1->parent = node;
 	node->right = node2, node2->parent = node;
