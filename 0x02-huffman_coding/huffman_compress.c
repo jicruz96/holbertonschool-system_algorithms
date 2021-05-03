d7107d2941f53c84b51eb508c82fbc366cbbc50c
J.I. Cruz <acounts@jicruz.com>
2021-05-03 10:04:44 -0500

    Add boiler plate for the advanced task compression program

---
+++ 0x02-huffman_coding/huffman_compress.c
@@ -0,0 +1,78 @@
+#include "heap/heap.h"
+#include "huffman.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define READ_SIZE 1
+/**
+ * compress - compresses data from in_fd and saves into out_fd
+ * @in_fd: input file descriptor
+ * @out_fd: output file descriptor
+ **/
+void compress(int in_fd, int out_fd)
+{
+	(void)in_fd;
+	(void)out_fd;
+}
+
+/**
+ * decompress - decompresses in_fd and saves result into out_fd
+ * @in_fd: input file descriptor
+ * @out_fd: output file descriptor
+ **/
+void decompress(int in_fd, int out_fd)
+{
+	(void)in_fd;
+	(void)out_fd;
+}
+
+/**
+ * main - entry point for a huffman tree compression/decompression program
+ * @argc: argument count
+ * @argv: arguments array
+ * Return: EXIT_SUCCESS on success | EXIT_FAILURE on failure
+ **/
+int main(int argc, char *argv[])
+{
+	int in_fd, out_fd;
+
+	if (argc != 4)
+	{
+		printf("Usage: huffman <mode> <filename> <out_filename>\n");
+		return (EXIT_FAILURE);
+	}
+
+	if (strcmp(argv[1], "c") == 0 || strcmp(argv[1], "d") == 0)
+	{
+		in_fd = open(argv[2], O_RDONLY);
+		if (in_fd == -1)
+		{
+			printf("No such file: %s\n", argv[2]);
+			return (EXIT_FAILURE);
+		}
+
+		out_fd = open(argv[3], O_WRONLY | O_CREAT | O_EXCL);
+		if (out_fd == -1)
+		{
+			printf("File already exists: %s\n", argv[3]);
+			close(in_fd);
+			return (EXIT_FAILURE);
+		}
+
+		if (argv[1][0] == 'c')
+			compress(in_fd, out_fd);
+		else
+			decompress(in_fd, out_fd);
+
+		close(in_fd);
+		close(out_fd);
+		return (EXIT_SUCCESS);
+	}
+
+	printf("Mode must either be 'c' (compress) or 'd' (decompress)\n");
+	return (EXIT_FAILURE);
+
+}
