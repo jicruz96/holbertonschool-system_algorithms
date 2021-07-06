0cc2956f0d64d4d965df610b0a145cb40e55b96a
jicruz96 <accounts@jicruz.com>
2021-07-05 23:47:21 -0500

    First go at task 2

---
+++ 0x04-nary_trees/2-nary_tree_traverse.c
@@ -0,0 +1,48 @@
+#include "nary_trees.h"
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+static size_t helper(
+	nary_tree_t const *root,
+	void (*action)(nary_tree_t const *node, size_t depth),
+	size_t depth
+);
+
+/**
+ * nary_tree_traverse - traverses an N-ary tree, executing the function
+ *                      `action` at each node
+ *
+ * @root: pointer to root of tree
+ * @action: action to perform on each node of tree
+ * Return: max depth of tree
+ */
+size_t nary_tree_traverse(
+	nary_tree_t const *root,
+	void (*action)(nary_tree_t const *node, size_t depth)
+)
+{
+	return helper(root, action, 0);
+}
+
+static size_t helper(
+	nary_tree_t const *root,
+	void (*action)(nary_tree_t const *node, size_t depth),
+	size_t depth
+)
+{
+	size_t tmp_depth, max_depth = 0;
+	nary_tree_t *tmp;
+
+	if (!root)
+		return (depth - 1);
+
+	action(root, depth);
+
+	for (tmp = root->children; tmp; tmp = tmp->next)
+	{
+		tmp_depth = helper(tmp, action, depth + 1);
+		max_depth = max(tmp_depth, max_depth);
+	}
+
+	return (max(max_depth, depth));
+}
